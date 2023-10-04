b16e3540c908d438b8581cf7bf5b7d8ab129c4df
Cholaml <220167529@stu.vtc.edu.hk>
2023-10-04 15:18:26 +0800

    added MainActivity

--- FYP_android/app/src/main/java/com/example/FYP/UI/Login.java
+++ FYP_android/app/src/main/java/com/example/FYP/UI/Login.java
@@ -1,4 +1,4 @@
-package com.example.FYP;
+package com.example.FYP.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,9 +7,10 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.FYP.R;
 import com.google.android.material.button.MaterialButton;
 
-public class MainLogin extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +18,10 @@ public class MainLogin extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
+        TextView username =(TextView) findViewById(R.id.etUsername);
+        TextView password =(TextView) findViewById(R.id.etPassword);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.btnLogin);
 
         //admin and admin
 
@@ -29,10 +30,10 @@ public class MainLogin extends AppCompatActivity {
             public void onClick(View v) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     //correct
-                    Toast.makeText(MainLogin.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
                 }else
                     //incorrect
-                    Toast.makeText(MainLogin.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Login.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
             }
         });
 
