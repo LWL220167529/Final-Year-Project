808fc944c257bd37685c03dc69fbec213d07e851
LWL220167529 <220167529@stu.vtc.edu.hk>
2024-02-22 22:59:18 +0800

    "web server is running on port 8000"

--- api/web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.contrib import admin
-from . import views
-
-# URLConf for the web app
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- api/web/web.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import *
-from . import views
-
-def index(request):
-    return render(request, 'views/index.html')
