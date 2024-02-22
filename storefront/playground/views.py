e8a307275ed80d5eeb3eee6af80190dbea4a673d
LWL220167529 <220167529@stu.vtc.edu.hk>
2024-02-23 00:17:09 +0800

    "fix the path can't be found issue"

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import sys
+import os
+sys.path.append(os.path.abspath(__file__).replace('\\storefront\\playground\\'+os.path.basename(os.path.abspath(__file__)), ''))  # Add the directory containing the 'api' module
 from api import *
 
 # Create your views here.
