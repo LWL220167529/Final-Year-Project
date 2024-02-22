4a823f9ef3bf61c09a7b909a42d2b86e64396d38
LWL220167529 <220167529@stu.vtc.edu.hk>
2024-02-23 00:31:49 +0800

    "tired of writing commit messages"

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import sys
-import os
-sys.path.append(os.path.abspath(__file__).replace('\\storefront\\playground\\'+os.path.basename(os.path.abspath(__file__)), ''))  # Add the directory containing the 'api' module
-from api import *
 
 # Create your views here.
 def index(request):
