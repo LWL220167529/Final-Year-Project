e88f21b09f65436dd7de3ecad9ab1a9c0c30fc0a
LWL220167529 <220167529@stu.vtc.edu.hk>
2024-03-25 23:46:55 +0800

    "idk fuck this work"

--- api/app.py
+++ api/app.py
@@ -3,10 +3,21 @@ import importlib
 from flask import Flask
 # from controller import __init__, index, cookies, plan
 
+views = [
+    'collection.py', 
+    'cookies.py', 
+    'index.py', 
+    'login.py', 
+    'plan.py', 
+    'schedule.py', 
+    'user.py', 
+    'view.py', 
+    '__init__.py'
+]
+
 def create_app():
     app = Flask(__name__)
 
-    views = [f for f in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'controller')) if f.endswith(".py")]
     for view in views:
         module = importlib.import_module(f"controller.{view[:-3]}")
         app.register_blueprint(module.bp)
--- api/test.py
+++ api/test.py
@@ -2,6 +2,9 @@ import os
 
 print(os.path.basename(__file__))
 
+views = [f for f in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'controller')) if f.endswith(".py")]
+
+print(views)
 # print(os.path.abspath(__file__))
 
 # print(os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'controller'))) # /home/runner/Flask-Blueprints/api
